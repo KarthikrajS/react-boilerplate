0671555694dac4d4d3958918d1554d917b45d6f3
Karthikraj Selvaraj <karthikrajselvaraj@Karthikrajs-MacBook-Air.local>
2024-11-25 16:44:57 +0530

    create boilerplate fix

--- create-boilerplate.js
+++ create-boilerplate.js
@@ -2,7 +2,7 @@
 
 const { execSync } = require('child_process');
 
-const repoUrl = 'https://github.com/KarthikrajS/node-boilerplate.git'; // Replace with your repo URL
+const repoUrl = 'https://github.com/KarthikrajS/react-boilerplate.git'; // Replace with your repo URL
 const projectName = process.argv[2] || 'my-project';
 
 console.log(`Cloning the boilerplate into ${projectName}...`);
